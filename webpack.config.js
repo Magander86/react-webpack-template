db1703f02e938ac2c6911771aa809cb5c9c73f30
Anderson Magal <78833874+Magander86@users.noreply.github.com>
2022-03-16 14:36:33 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,34 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {    
-    entry: path.resolve(__dirname, "./frontend/src/index.js"),
+    entry: path.resolve(__dirname, "./src/index.js"),
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: ["babel-loader"]
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
             }
         ]
     },
     resolve: {
-        extensions: ["*", ".js", ".jsx"]
+        extensions: ["*", ".js", ".jsx",".css"]
     },    
     output: {
-        path: path.resolve(__dirname, "./frontend/public"),
-        filename: "main.js"
+        path: path.resolve(__dirname, "./public"),
+        filename: "main.js",        
     },
     plugins: [new webpack.HotModuleReplacementPlugin()],
     devServer: {
-        static: path.resolve(__dirname, "./frontend/public"),
+        static: path.resolve(__dirname, "./public"),
+        historyApiFallback: true, 
         hot: true,
     }
 }
